08deaf6e855259948e05b76e429455889ed1170a
Linus Lee <linus@thesephist.com>
2020-09-08 14:24:42 -0400

    Add basics of a lexer

--- src/lex.rs
+++ src/lex.rs
@@ -1,14 +1,16 @@
-#[derive(Debug)]
-pub enum Tok {
+use std::fmt;
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum TokKind<'s> {
     Separator,
 
-    Comment(String),
+    Comment(&'s str),
 
-    Ident(String),
+    Ident(&'s str),
     EmptyIdent,
 
     NumberLiteral(f64),
-    StringLiteral(String),
+    StringLiteral(&'s str),
 
     TrueLiteral,
     FalseLiteral,
@@ -45,6 +47,206 @@ pub enum Tok {
     RBrace,
 }
 
+#[derive(Debug, Clone)]
+pub struct Span(usize, usize);
+
+#[derive(Debug, Clone)]
+pub struct Tok<'s> {
+    kind: TokKind<'s>,
+    span: Span,
+    source: &'s str,
+}
+
+impl fmt::Display for Tok<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{:?} [{}]",
+            self.kind,
+            &self.source[self.span.0..self.span.1]
+        )
+    }
+}
+
+// TODO: support producing Spans with line/column info
+#[derive(Debug)]
+pub struct Reader<'s> {
+    source: &'s str,
+    start: usize,
+    index: usize,
+}
+
+// impl should be more efficient. In particular, peek()
+// should support seeking thru Unicode source text in constant time.
+impl<'s> Reader<'s> {
+    fn new(source: &str) -> Reader {
+        return Reader {
+            source,
+            start: 0,
+            index: 0,
+        };
+    }
+
+    fn peek(&self) -> char {
+        return self
+            .source
+            .chars()
+            .nth(self.index)
+            .expect("Reader index out of bounds");
+    }
+
+    fn next(&mut self) {
+        if self.has_next() {
+            self.index += 1;
+        }
+    }
+
+    fn has_next(&self) -> bool {
+        return self.source.len() > self.index;
+    }
+
+    fn pop_span(&mut self) -> Span {
+        let span = Span(self.start, self.index);
+        self.start = self.index;
+        return span;
+    }
+
+    fn pop_token(&mut self, kind: TokKind<'s>) -> Tok<'s> {
+        return Tok {
+            kind: kind,
+            span: self.pop_span(),
+            source: self.source,
+        };
+    }
+
+    fn pop_token_and_next(&mut self, kind: TokKind<'s>) -> Tok<'s> {
+        self.next();
+        return Tok {
+            kind: kind,
+            span: self.pop_span(),
+            source: self.source,
+        };
+    }
+
+    fn take_while<F>(&mut self, cond: F) -> &str
+    where
+        F: Fn(char) -> bool,
+    {
+        while cond(self.peek()) {
+            self.next();
+        }
+        return self.take();
+    }
+
+    fn take(&self) -> &str {
+        return &self.source[self.start..self.index];
+    }
+}
+
 pub fn tokenize(prog: &str) -> Vec<Tok> {
-    return Vec::<Tok>::new();
+    // TODO: compile error here b/c `tokens` outlives reader,
+    // but data from reader flows into tokens.
+    let mut tokens = Vec::<Tok>::new();
+    let mut reader = Reader::new(prog);
+
+    fn ensure_separator<'s>(tokens: &mut Vec<Tok<'s>>, reader: &mut Reader<'s>) {
+        match tokens.last() {
+            Some(tok) => {
+                if tok.kind != TokKind::Separator {
+                    tokens.push(reader.pop_token(TokKind::Separator));
+                }
+            }
+            None => return,
+        };
+    }
+
+    while reader.has_next() {
+        let c = reader.peek();
+
+        match c {
+            '\'' => {
+                println!("start of string");
+                reader.next();
+            }
+            '`' => {
+                println!("start of comment");
+                reader.next();
+            }
+            '\n' => {
+                ensure_separator(&mut tokens, &mut reader);
+                reader.next();
+                reader.pop_span();
+            }
+            '\t' => {
+                reader.next();
+                reader.pop_span();
+            }
+            ' ' => {
+                reader.next();
+                reader.pop_span();
+            }
+            '_' => tokens.push(reader.pop_token_and_next(TokKind::EmptyIdent)),
+            '~' => tokens.push(reader.pop_token_and_next(TokKind::NegOp)),
+            '+' => tokens.push(reader.pop_token_and_next(TokKind::AddOp)),
+            '*' => tokens.push(reader.pop_token_and_next(TokKind::MulOp)),
+            '/' => tokens.push(reader.pop_token_and_next(TokKind::DivOp)),
+            '%' => tokens.push(reader.pop_token_and_next(TokKind::ModOp)),
+            '&' => tokens.push(reader.pop_token_and_next(TokKind::AndOp)),
+            '|' => tokens.push(reader.pop_token_and_next(TokKind::OrOp)),
+            '^' => tokens.push(reader.pop_token_and_next(TokKind::XorOp)),
+            '<' => tokens.push(reader.pop_token_and_next(TokKind::LtOp)),
+            '>' => tokens.push(reader.pop_token_and_next(TokKind::GtOp)),
+            ',' => tokens.push(reader.pop_token_and_next(TokKind::Separator)),
+            '.' => tokens.push(reader.pop_token_and_next(TokKind::AccessorOp)),
+            '(' => tokens.push(reader.pop_token_and_next(TokKind::LParen)),
+            ')' => tokens.push(reader.pop_token_and_next(TokKind::RParen)),
+            '[' => tokens.push(reader.pop_token_and_next(TokKind::LBracket)),
+            ']' => tokens.push(reader.pop_token_and_next(TokKind::RBracket)),
+            '{' => tokens.push(reader.pop_token_and_next(TokKind::LBrace)),
+            '}' => tokens.push(reader.pop_token_and_next(TokKind::RBrace)),
+            ':' => {
+                reader.next();
+                match reader.peek() {
+                    ':' => {
+                        tokens.push(reader.pop_token_and_next(TokKind::MatchColon));
+                    }
+                    '=' => {
+                        tokens.push(reader.pop_token_and_next(TokKind::MatchColon));
+                    }
+                    _ => tokens.push(reader.pop_token_and_next(TokKind::KeyValueSeparator)),
+                }
+            }
+            '=' => {
+                reader.next();
+                match reader.peek() {
+                    '>' => {
+                        tokens.push(reader.pop_token_and_next(TokKind::FunctionArrow));
+                    }
+                    _ => tokens.push(reader.pop_token_and_next(TokKind::EqOp)),
+                }
+            }
+            '-' => {
+                reader.next();
+                match reader.peek() {
+                    '>' => {
+                        tokens.push(reader.pop_token_and_next(TokKind::CaseArrow));
+                    }
+                    _ => tokens.push(reader.pop_token_and_next(TokKind::SubOp)),
+                }
+            }
+            '0'..='9' => {
+                let num = reader.take_while(|c| c >= '0' && c <= '9' || c == '.');
+                // TODO: actually parse the number
+                tokens.push(reader.pop_token(TokKind::NumberLiteral(0.42)));
+            }
+            _ => {
+                // TODO support full unicode
+                let ident = reader
+                    .take_while(|c| c.is_ascii_alphanumeric() || c == '?' || c == '!' || c == '@');
+                tokens.push(reader.pop_token(TokKind::Ident("hi")));
+            }
+        }
+    }
+
+    return tokens;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::fs;
+use std::path::PathBuf;
+
 mod args;
 mod lex;
 
@@ -7,7 +10,7 @@ fn main() {
     let opts = args::get_cli_opts();
 
     match opts.action {
-        args::Action::Eval(mode) => println!("eval mode {:?}", mode),
+        args::Action::Eval(mode) => eval(mode),
         args::Action::Version => print_version(),
         args::Action::Help => print_help(),
     }
@@ -19,4 +22,31 @@ fn print_version() {
 
 fn print_help() {
     println!("help...");
+}
+
+fn eval(mode: args::EvalMode) {
+    match mode {
+        args::EvalMode::RunFile(path) => eval_file(path),
+        args::EvalMode::Eval(prog) => eval_string(prog),
+        args::EvalMode::Repl => println!("repl"),
+    }
+}
+
+fn eval_file(path: PathBuf) {
+    let file = match fs::read_to_string(path) {
+        Ok(prog) => prog,
+        Err(e) => {
+            eprintln!("error: {:?}", e);
+            std::process::exit(1);
+        }
+    };
+
+    eval_string(file);
+}
+
+fn eval_string(prog: String) {
+    let tokens = lex::tokenize(&prog);
+    for tok in tokens.iter() {
+        println!("{}", tok);
+    }
 }
